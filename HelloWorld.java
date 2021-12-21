d8ee095ccc231d53a06e491261facff245782aef
Barkha-Hasija <96294262+Barkha-Hasija@users.noreply.github.com>
2021-12-21 17:22:42 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.cg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world of Jenkin");
+		System.out.println("Hello world of Jenkin");
+		
+
+	}
+
+}
